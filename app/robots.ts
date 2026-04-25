a1ba8d49abf4a9d4a3ac4eb9b86338d50fcaab07
Alseptkg <alseptkg@gmail.com>
2026-04-25 20:26:29 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,13 @@
+import type { MetadataRoute } from 'next'
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://senatian.ru/sitemap.xml',
+  }
+}
