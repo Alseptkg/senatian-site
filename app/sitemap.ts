bb5b73579cba531da3f1b359465b52b6c73be75a
Alseptkg <alseptkg@gmail.com>
2026-04-25 20:30:40 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,14 @@
-import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://senatian.ru',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-  ]
-}
+return [
+  {
+    url: 'https://senatian.ru',
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 1,
+  },
+  {
+    url: 'https://www.senatian.ru',
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.9,
+  },
+]
